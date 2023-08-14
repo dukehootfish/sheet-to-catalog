2b9ebb2926a2ae467ea993cc05a6fe7ae4b3cea6
dukehootfish <77045140+dukehootfish@users.noreply.github.com>
2023-08-14 03:29:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
-const spreadsheetId = '15DkWp1UTI7varIFLKMVa13JGlpz6gIE1Zd9EZRU7_BQ';
-const apiKey = 'AIzaSyBR2WBVvpeL3uB_dlua8UGFWAPYYyruPVg;
-const range = 'sheety 11'; // Change this to your sheet's name
+const itemsContainer = document.getElementById('items-container');
 
-function fetchData() {
-    const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`;
-    return fetch(url)
-        .then(response => response.json())
-        .then(data => data.values);
-}
+// Replace with your Google Sheets URL
+const sheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQiD4YbQLlsyn8IYJwA4QgKaVW8U7yToBg57duch6Akz_iuOMsZmYz-rTjuc54snxSMPfSTJEbxinyQ/pubhtml';
 
-function displayData(data) {
-    const catalogDiv = document.getElementById('catalog');
-    let catalogHTML = '<h1>Catalog</h1>';
-    
-    data.forEach(item => {
-        const [name, description, imageURL] = item;
-        catalogHTML += `
-            <div class="item">
-                <img src="${imageURL}" alt="${name}">
-                <h2>${name}</h2>
-                <p>${description}</p>
-            </div>
-        `;
+fetch(sheetUrl)
+    .then(response => response.text())
+    .then(data => {
+        const rows = data.split('\n');
+        const headers = rows[0].split(',');
+
+        for (let i = 1; i < rows.length; i++) {
+            const values = rows[i].split(',');
+            const item = {};
+
+            for (let j = 0; j < headers.length; j++) {
+                item[headers[j]] = values[j];
+            }
+
+            const itemElement = createItemElement(item);
+            itemsContainer.appendChild(itemElement);
+        }
     });
 
-    catalogDiv.innerHTML = catalogHTML;
-}
+function createItemElement(item) {
+    const itemDiv = document.createElement('div');
+    itemDiv.className = 'item';
 
-fetchData()
-    .then(displayData)
-    .catch(error => console.error('Error fetching data:', error));
+    const image = document.createElement('img');
+    image.src = item['Image'];
+    itemDiv.appendChild(image);
+
+    const itemName = document.createElement('h2');
+    itemName.textContent = item['Item Name'];
+    itemDiv.appendChild(itemName);
+
+    const description = document.createElement('p');
+    description.textContent = item['Description'];
+    itemDiv.appendChild(description);
+
+    const price = document.createElement('p');
+    price.textContent = 'Price: $' + item['Price'];
+    itemDiv.appendChild(price);
+
+    return itemDiv;
+}
